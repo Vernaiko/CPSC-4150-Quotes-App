de03dc36700608fc826ca0f50f58aff3e8504cc7
Vernaiko <wlovin@clemson.edu>
2025-08-28 14:58:23 -0400

    Removed boilerplate comments from the tutorial series to remove clutter.

--- lib/quote.dart
+++ lib/quote.dart
@@ -6,23 +6,6 @@ class Quote {
   String sourceMaterial;
   String character;
 
-  //  normal constructor, as we've already seen
-
-  //  Quote(String author, String text){
-  //    this.text = text;
-  //    this.author = author;
-  //  }
-
-  //  constructor with named parameters
-
-  //  Quote({ String author, String text }){
-  //    this.text = text;
-  //    this.author = author;
-  //  }
-
-  // constructor with named parameters
-  // & automatically assigns named arguments to class properties
-
   Quote({ required this.text, required this.author, required this.category,
   required this.character, required this.sourceMaterial});
 
